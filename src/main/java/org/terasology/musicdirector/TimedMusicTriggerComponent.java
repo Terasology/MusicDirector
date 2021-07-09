ea5b7b9a56951e03a72f738271a1aab79b622c07
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:16 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/musicdirector/TimedMusicTriggerComponent.java
+++ src/main/java/org/terasology/musicdirector/TimedMusicTriggerComponent.java
@@ -1,27 +1,14 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.musicdirector;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * A music trigger that is based on a daily time interval.
  */
-public final class TimedMusicTriggerComponent implements Component  {
+public final class TimedMusicTriggerComponent implements Component<TimedMusicTriggerComponent>  {
 
     public float dailyStart;
     public float dailyEnd;
@@ -31,5 +18,12 @@ public final class TimedMusicTriggerComponent implements Component  {
     @Override
     public String toString() {
         return String.format("MusicTrigger [%.2f...%.2f -> '%s']", dailyStart, dailyEnd, assetUri);
+    }
+
+    @Override
+    public void copy(TimedMusicTriggerComponent other) {
+        this.dailyStart = other.dailyStart;
+        this.dailyEnd = other.dailyEnd;
+        this.assetUri = other.assetUri;
     }
 }
